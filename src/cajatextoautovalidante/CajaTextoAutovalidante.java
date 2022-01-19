841c827998ec0be6c24c956a3b52583129cb529a
Pedro Jorquera Hoyas <7099@iesluisbraille.es>
2022-01-19 09:46:40 +0100

    add variables

--- src/cajatextoautovalidante/CajaTextoAutovalidante.java
+++ src/cajatextoautovalidante/CajaTextoAutovalidante.java
@@ -18,11 +18,13 @@ import javax.swing.JTextField;
  *
  * @author pedrr
  */
-public class CajaTextoAutovalidante extends JPanel implements Serializable{
+public class CajaTextoAutovalidante extends JPanel implements Serializable {
 
     public String tipoValidacion;
     private JLabel mensajeValidacion;
     private JTextField cajaTexto;
+    private boolean correccion;
+    private String mensajesError;
 
     public CajaTextoAutovalidante() {
         mensajeValidacion = new JLabel();
@@ -42,6 +44,8 @@ public class CajaTextoAutovalidante extends JPanel implements Serializable{
                 if (tipoValidacion == "dni" || tipoValidacion == "DNI") {
                     if (cajaTexto.getText().length() != 9) {
                         mensajeValidacion.setText("El DNI debe tener 9 digitos");
+                        correccion = false;
+                        mensajesError = "El DNI debe tener 9 digitos";
                     } else {
                         try {
                             String DNI = cajaTexto.getText().substring(0, 8);
@@ -50,24 +54,34 @@ public class CajaTextoAutovalidante extends JPanel implements Serializable{
                                 char letra = cajaTexto.getText().charAt(8);
                                 Integer.parseInt(letra + "");
                                 mensajeValidacion.setText("El ultimo digito debe ser una letra");
+                                correccion = false;
+                                mensajesError = "El ultimo digito debe ser una letra";
                             } catch (NumberFormatException w) {
 
                                 mensajeValidacion.setText("Correcto");
+                                correccion = true;
                             }
                         } catch (NumberFormatException w) {
                             mensajeValidacion.setText("Los primeros 8 digitos deben ser numeros y el ultimo una letra");
+                            correccion = false;
+                            mensajesError = "Los primeros 8 digitos deben ser numeros y el ultimo una letra";
                         }
                     }
                 }
                 if (tipoValidacion == "cp" || tipoValidacion == "CP") {
                     if (cajaTexto.getText().length() != 5) {
                         mensajeValidacion.setText("El codigo postal debe tener 5 digitos");
+                        correccion = false;
+                        mensajesError = "El codigo postal debe tener 5 digitos";
                     } else {
                         try {
                             Integer.parseInt((cajaTexto.getText()));
                             mensajeValidacion.setText("Correcto");
+                            correccion = true;
                         } catch (NumberFormatException w) {
                             mensajeValidacion.setText("El codigo postal no puede tener letras");
+                            correccion = false;
+                            mensajesError = "El codigo postal no puede tener letras";
                         }
                     }
                 }
@@ -75,12 +89,17 @@ public class CajaTextoAutovalidante extends JPanel implements Serializable{
                     {
                         if (cajaTexto.getText().length() != 9) {
                             mensajeValidacion.setText("El teléfono debe tener 9 digitos");
+                            correccion = false;
+                            mensajesError = "El codigo postal no puede tener letras";
                         } else {
                             try {
                                 Integer.parseInt(cajaTexto.getText());
-                            mensajeValidacion.setText("Correcto");
+                                mensajeValidacion.setText("Correcto");
+                                correccion = true;
                             } catch (NumberFormatException w) {
                                 mensajeValidacion.setText("El teléfono solo puede contener números");
+                                correccion = false;
+                                mensajesError = "El teléfono solo puede contener números";
                             }
                         }
                     }
@@ -94,12 +113,30 @@ public class CajaTextoAutovalidante extends JPanel implements Serializable{
 
     }
 
+    
+   //getters & setters para utilizar el mensaje de error y si es correcto o no fuera
     public String getTipoValidacion() {
         return tipoValidacion;
     }
 
     public void setTipoValidacion(String tipoValidacion) {
         this.tipoValidacion = tipoValidacion;
+    }
+
+    public boolean isCorreccion() {
+        return correccion;
+    }
+
+    public void setCorreccion(boolean correccion) {
+        this.correccion = correccion;
+    }
+
+    public String getMensajesError() {
+        return mensajesError;
+    }
+
+    public void setMensajesError(String mensajesError) {
+        this.mensajesError = mensajesError;
     }
 
 }
